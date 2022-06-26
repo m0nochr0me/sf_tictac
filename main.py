12d573b4969f7e4d2a706a5ccea2b47440ef285d
m0nochr0mex <m0nochr0mex@gmail.com>
2022-06-27 07:27:44 +0800

    Updated comments

--- main.py
+++ main.py
@@ -25,10 +25,10 @@ def cls():
 def check_win(board, mark='X'):
     """This abomination returns True if any win condition is satisfied for given mark"""
 
-    # check every tile in every row
-    # check every tile in every column
-    # check tiles on forward diagonal
-    # check files on backward diagonal
+    # Check every tile in every row
+    # Check every tile in every column
+    # Check tiles on forward diagonal
+    # Check files on backward diagonal
     return any(mark in b and b[:-1] == b[1:] for b in board) or \
         any(all(mark in b[i] and b[i][:-1] == b[i][1:] for b in board) for i in range(3)) or \
         all(mark in b and b[:-1] == b[1:] for b in [board[i][i] for i in range(3)]) or \
@@ -52,37 +52,37 @@ def mark_tile(board, tile, mark='X'):
 def ai(board, mark='O', difficulty=1):
     """The [in]famous Artificial Intelligence"""
 
-    possible_tiles = []  # all possible tiles
-    counter_tiles = []  # tiles to counter human winning
-    winning_tiles = []  # tiles leading to instant AI win
+    possible_tiles = []  # All possible tiles
+    counter_tiles = []  # Tiles to counter human winning
+    winning_tiles = []  # Tiles leading to instant AI win
     for i in range(1, 10):
-        # copy current board twice -_-
-        # sometimes I think it wasn't good idea to use matrix for board
-        # but now it's too late.
+        # Copy current board twice -_-
+        # Sometimes I think it wasn't good idea to use matrix for board
+        # But now it's too late.
         new_board = deepcopy(board)
         counter_board = deepcopy(board)
         try:
-            # check all possible, possible winning, and possible counter tiles
+            # Check all possible, possible winning, and possible counter tiles
             new_board = mark_tile(new_board, i, mark)
             counter_board = mark_tile(counter_board, i, 'X' if mark == 'O' else 'O')
         except ValueError:
             continue
         else:
-            # populate corresponding lists
+            # Populate corresponding lists
             possible_tiles.append(i)
             if check_win(counter_board, 'X' if mark == 'O' else 'O'):
                 counter_tiles.append(i)
             if check_win(new_board, mark):
                 winning_tiles.append(i)
 
-    # difficulty here is leaving space for AI to make errors so human can win
-    # always prefer wining tiles
+    # Difficulty here is leaving space for AI to make errors so human can win
+    # Always prefer wining tiles
     if len(winning_tiles) > 0 and randint(1, 4) <= difficulty:
         return mark_tile(board, winning_tiles[0], mark)
-    # if AI can't win, check if it can prevent human winning
+    # If AI can't win, check if it can prevent human winning
     elif len(counter_tiles) > 0 and randint(1, 4) <= difficulty:
         return mark_tile(board, counter_tiles[0], mark)
-    # mark random tiles if none of the above
+    # Mark random tiles if none of the above
     else:
         return mark_tile(board, choice(possible_tiles), mark)
 
@@ -102,10 +102,11 @@ def get_cmd(mark='?'):
         cmd = input(f'?{mark}>')
         cmd_split = cmd.split()
     except KeyboardInterrupt:
+        # Catch Ctrl+C
         print('Ok Bye!')
         sys.exit(0)
     else:
-        # always tuple for empty command to get rid of additional checks
+        # Always return tuple for empty command to avoid additional checks
         return cmd_split or ('x',)
 
 
@@ -138,13 +139,13 @@ def main():
         cmd = get_cmd(player_mark)
 
         cls()
-        # using startswith so it is unnecessary to type full command
+        # Using startswith so it is unnecessary to type full command
         if cmd[0].startswith('n'):
             # New game
             board = reset_board()
             game_state = 1
             turn = 0
-            # set human mark to X or O
+            # Set human mark to X or O
             if len(cmd) > 1 and cmd[1] in 'XO':
                 player_mark = cmd[1]
         elif cmd[0].startswith('d'):
@@ -158,7 +159,7 @@ def main():
                 continue
             if len(cmd) > 1 and cmd[1] in '123456789':
                 try:
-                    # if tile can be marked, mark it, and give turn to ai
+                    # If tile can be marked, mark it, and give turn to ai
                     board = mark_tile(board, int(cmd[1]), player_mark)
                     turn += 1
                     game_state = 2
@@ -193,6 +194,7 @@ def main():
                 ai_score += 1
                 print('AI win')
 
+        # Show current board
         print_board(board, human_score, ai_score, difficulty)
 
 
